e71b6129beea8777ea28ebee805d92746fbb011d
Leo McArdle <leomcardle@gmail.com>
2017-10-08 10:32:25 +0100

    refactor

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,7 @@ var request = require('request')
 var maps = require('@google/maps')
 var util = require('util')
 var moment = require('moment')
-var Redis = require('ioredis')
-var redis = new Redis()
-var dedupe = require('dedupe')
-
-function cache_get_or_set(key, get_value, callback) {
-  redis.get(key, (err, res) => {
-    if (err) callback(err, null)
-    if (!res) {
-      get_value(value => {
-        redis.set(key, value)
-        callback(null, value)
-      })
-    } else {
-      callback(null, res)
-    }
-  })
-}
+var Timetable = require('../lib/timetable')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -33,68 +17,57 @@ router.get('/', function(req, res, next) {
   }
 });
 
-router.post('/', (req, res, next) => {
-  var b = req.body
-  if (moment().day() == b.day) {
+function next_day (day) {
+  if (moment().day() == day) {
     var date = moment()
-  } else if (moment().day() <= b.day) {
-    var date = moment().day(b.day)
+  } else if (moment().day() <= day) {
+    var date = moment().day(day)
   } else {
-    var date = moment().add(1, 'week').day(b.day)
+    var date = moment().add(1, 'week').day(day)
   }
-  console.log('date', date.format('YYYY-MM-DD'))
+  return date
+}
 
+router.post('/', (req, res, next) => {
   var token = req.cookies.token
-  var timetable_query = `${config.base_url}/timetable/personal` +
-                        `?token=${token}` +
-                        `&client_secret=${config.client_secret}`
-  console.log(timetable_query)
+  var timetable = new Timetable({ base_url: config.base_url,
+                                  client_secret: config.client_secret,
+                                  token: token })
 
-  cache_get_or_set(timetable_query, callback => {
-    request(timetable_query, (err, reqRes, body) => {
+  var b = req.body
+  var day_date = next_day(b.day).format('YYYY-MM-DD')
+
+  timetable.for_day(day_date, (err, day_timetable) => {
+    var first_class = day_timetable.first_class()
+
+    var class_location = first_class.location.address.join(' ')
+    var class_time = moment(`${day_date} ${first_class.start_time}`)
+    console.log('class_location', class_location)
+    console.log('class_time', class_time)
+
+    var mapsClient = maps.createClient({ key: config.google_key })
+
+    mapsClient.distanceMatrix({
+      origins: b.location,
+      destinations: 'WC1E 6BT',
+      mode: 'walking'
+    }, (err, mRes) => {
       if (err) {
-        res.status(500).send('Timetable API failed')
+        res.status(500).send(`Google Maps API failed: ${err.json.error_message}`)
       } else {
-        callback(body)
+        var duration = mRes.json.rows[0].elements[0].duration
+        var wake_time = class_time.subtract(b.extra, 'minutes')
+                                  .subtract(duration.value, 'seconds')
+                                  
+        res.render('calculation', {
+          travel_time: duration.text,
+          extra_time: b.extra,
+          wake_time: wake_time.format('h:mm a')
+        })
       }
     })
-  }, (err, value) => {
-    if (err) {
-      res.status(500).send('Redis failed')
-    } else {
-      var day_timetable = JSON.parse(value).timetable[date.format('YYYY-MM-DD')]
-      day_timetable = dedupe(day_timetable, x => x.start_time)
-      day_timetable.sort((a, b) => {
-        if (a.start_time < b.start_time) return -1
-        if (a.start_time > b.start_time) return 1
-        return 0
-      })
 
-      var class_location = day_timetable[0].location.address.join(' ')
-      var class_time = moment(`${date.format('YYYY-MM-DD')} ${day_timetable[0].start_time}`)
-      console.log('class_time', class_time)
-
-      var mapsClient = maps.createClient({ key: config.google_key })
-
-      mapsClient.distanceMatrix({
-        origins: b.location,
-        destinations: 'WC1E 6BT',
-        mode: 'walking'
-      }, (err, mRes) => {
-        if (err) {
-          res.status(500).send(`Google Maps API failed: ${err.json.error_message}`)
-        } else {
-          // console.log(util.inspect(mRes.json, { depth: null }))
-          var duration = mRes.json.rows[0].elements[0].duration
-
-          var wake_time = class_time.subtract(duration.value, 'minutes')
-
-          res.render('calculation', { travel_time: duration.text, wake_time: wake_time.format('h:mm a') })
-        }
-      })
-    }
   })
-
 })
 
 module.exports = router;
