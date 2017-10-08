2f8c1aa2eb3d98580bbd25514a5f2a2bc36b3ea1
Leo McArdle <leomcardle@gmail.com>
2017-10-08 12:41:44 +0100

    fix everything

--- routes/index.js
+++ routes/index.js
@@ -42,32 +42,45 @@ router.post('/', (req, res, next) => {
   timetable.for_day(day_date, (err, day_timetable) => {
     var first_class = day_timetable.first_class()
 
-    var class_location = first_class.location.address.join(' ')
-    var class_time = moment(`${day_date} ${first_class.start_time}`)
-    console.log('class_location', class_location)
-    console.log('class_time', class_time)
+    if (first_class) {
+      first_class.location = first_class.location.address.join(' ')
+      first_class.time = moment(`${day_date} ${first_class.start_time}`)
 
-    var mapsClient = maps.createClient({ key: process.env.GOOGLE_KEY })
+      var mapsClient = maps.createClient({ key: process.env.GOOGLE_KEY })
 
-    mapsClient.distanceMatrix({
-      origins: b.location,
-      destinations: 'WC1E 6BT',
-      mode: 'walking'
-    }, (err, mRes) => {
-      if (err) {
-        res.status(500).send(`Google Maps API failed: ${err.json.error_message}`)
-      } else {
-        var duration = mRes.json.rows[0].elements[0].duration
-        var wake_time = class_time.subtract(b.extra, 'minutes')
-                                  .subtract(duration.value, 'seconds')
-
+      var location_blank = !first_class.location.trim()
+      if (location_blank) {
         res.render('calculation', {
-          travel_time: duration.text,
-          extra_time: b.extra,
-          wake_time: wake_time.format('h:mm a')
+          no_location: true,
+          first_class: first_class
+        })
+      } else {
+        mapsClient.distanceMatrix({
+          origins: b.location,
+          destinations: first_class.location,
+          mode: 'walking'
+        }, (err, mRes) => {
+          if (err) {
+            res.status(500).send(`Google Maps API failed: ${err.json.error_message}`)
+          } else {
+            var duration = mRes.json.rows[0].elements[0].duration
+            var wake_time = first_class.time.subtract(b.extra, 'minutes')
+                                            .subtract(duration.value, 'seconds')
+
+            res.render('calculation', {
+              first_class: first_class,
+              travel_time: duration.text,
+              extra_time: b.extra,
+              wake_time: wake_time.format('h:mm a')
+            })
+          }
         })
       }
-    })
+    } else {
+      res.render('calculation', {
+        no_class: true
+      })
+    }
 
   })
 })
